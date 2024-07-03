27f8469b64314390f9d27c52f3e8d5e309a27e6e
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-07-03 19:22:47 +0200

    KAN-5 Global Exception Handler

---
+++ BookABite_backend/API/GlobalExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.GlobalExceptionHandlers
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var path = $"{httpContext.Request.Method}{httpContext.Request.Path}";
+            await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+            {
+                Status = (int)HttpStatusCode.InternalServerError,
+                Type = exception.GetType().Name,
+                Title = "An unexpected error occured",
+                Detail = exception.Message,
+                Instance = $"{httpContext.Request.Method}{httpContext.Request.Path}"
+            }, cancellationToken: cancellationToken);
+
+            return true;
+        }
+    }
+}
