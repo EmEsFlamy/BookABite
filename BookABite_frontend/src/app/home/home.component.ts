1cc9d75e25d8fe16596770656d95d091693ca2e8
Lkem13 <91785810+Lkem13@users.noreply.github.com>
2024-08-12 22:09:07 +0200

    slideshow

--- BookABite_frontend/src/app/home/home.component.ts
+++ BookABite_frontend/src/app/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {CarouselModule} from '@coreui/angular';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule, CarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  slides = [
+    { src: 'assets/images/restaurant1.jpg', title: 'Delicious Dish 1', subtitle: 'Our Speciality' },
+    { src: 'assets/images/restaurant2.jpg', title: 'Delicious Dish 2', subtitle: 'Cooked to Perfection' },
+    { src: 'assets/images/restaurant3.jpg', title: 'Delicious Dish 3', subtitle: 'A Taste of Heaven' },
+    { src: 'assets/images/restaurant4.jpg', title: 'Delicious Dish 4', subtitle: 'Gourmet Delight' },
+    { src: 'assets/images/restaurant5.jpg', title: 'Delicious Dish 5', subtitle: 'Ultimate Flavor' }
+  ];
 }
