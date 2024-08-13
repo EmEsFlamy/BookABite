1ec01c298de5c37762a40a5b2d1a2088a3c64ffb
Lkem13 <91785810+Lkem13@users.noreply.github.com>
2024-08-13 11:41:39 +0200

    slideshow wip

--- BookABite_frontend/src/app/home/home.component.ts
+++ BookABite_frontend/src/app/home/home.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit} from '@angular/core';
-import {CarouselModule} from '@coreui/angular';
+import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import {
-  CarouselCaptionComponent,
-  CarouselComponent,
-  CarouselControlComponent,
-  CarouselIndicatorsComponent,
-  CarouselInnerComponent,
-  CarouselItemComponent,
-  ThemeDirective
-} from '@coreui/angular';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CarouselCaptionComponent, CarouselModule, CommonModule, ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent, CarouselControlComponent],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+}
 
-  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
 
-  ngOnInit(): void {
-    this.slides[0] = {
-      id: 0,
-      src: 'assets/images/restaurant1.jpg',
-      title: 'First slide',
-      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
-    };
-  }
+@Component({
+  selector: 'nz-demo-carousel-basic',
+  template: `
+    <nz-carousel [nzEffect]="effect">
+      @for (index of array; track index) {
+        <div nz-carousel-content>
+          <h3>{{ index }}</h3>
+        </div>
+      }
+    </nz-carousel>
+  `,
+  styles: [
+    `
+      [nz-carousel-content] {
+        text-align: center;
+        height: 160px;
+        line-height: 160px;
+        background: #364d79;
+        color: #fff;
+        overflow: hidden;
+      }
+
+      h3 {
+        color: #fff;
+        margin-bottom: 0;
+        user-select: none;
+      }
+    `
+  ]
+})
+export class NzDemoCarouselBasicComponent {
+  array = [1, 2, 3, 4];
+  effect = 'scrollx';
 }
