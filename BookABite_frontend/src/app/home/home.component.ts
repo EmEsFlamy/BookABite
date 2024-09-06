fb3f91e07e320746d58527efb4b1797f0814c350
Lkem13 <91785810+Lkem13@users.noreply.github.com>
2024-09-06 16:18:48 +0200

    section two-column-container

--- BookABite_frontend/src/app/home/home.component.ts
+++ BookABite_frontend/src/app/home/home.component.ts
@@ -1,25 +1,24 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ImportModule } from '../import.module';
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, ImportModule],
+  imports: [RouterModule, ImportModule, NgFor],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 
 
 export class HomeComponent {
-  /*slides = [
-    { src: 'assets/images/restaurant1.jpg'},
+  array = [
     { src: 'assets/images/restaurant2.jpg'},
     { src: 'assets/images/restaurant3.jpg'},
-  ]*/
-  array = [1, 2, 3, 4];
-  effect = 'scrollx';
+    { src: 'assets/images/restaurant5.jpg'},
+  ]
 }
 
 
