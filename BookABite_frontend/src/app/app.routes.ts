b3c3c642407eeced0b20f8a1ed258bea15c3f2e5
Lkem13 <91785810+Lkem13@users.noreply.github.com>
2024-10-06 20:26:26 +0200

    login page

--- BookABite_frontend/src/app/app.routes.ts
+++ BookABite_frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LoginComponent },
 ];
--- BookABite_frontend/src/app/import.module.ts
+++ BookABite_frontend/src/app/import.module.ts
@@ -1,9 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 const importedModules = [
     NzCarouselModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzButtonModule,
+    NzIconModule,
+    NzGridModule,
 ];
 
 @NgModule({
