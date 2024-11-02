f3186350473c90e3e73c52f714b7f50eef89ad6a
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-11-02 23:17:50 +0100

    KAN-31 TableRepository

---
+++ BookABite_backend/DOMAIN/Repositories/ITableRepository.cs
@@ -0,0 +1,18 @@
+﻿using DOMAIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMAIN.Repositories
+{
+    public interface ITableRepository
+    {
+        Task<Table> GetByIdAsync(int tableId);
+        Task<Table> CreateAsync(Table table);
+        Task<Table> UpdateAsync(Table table);
+        Task<bool> DeleteAsync(int tableId);
+        Task<List<Table>> GetAsync();
+    }
+}
