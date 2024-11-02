01307119749d4caa050815c24d5dbba3e3ca80d4
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-11-02 13:21:39 +0100

    KAN-29 UserService

--- BookABite_backend/APPLICATION/Services/UserService.cs
+++ BookABite_backend/APPLICATION/Services/UserService.cs
@@ -1,8 +1,38 @@
-﻿using DOMAIN.Services;
-
+﻿using DOMAIN.Models;
+using DOMAIN.Repositories;
+using DOMAIN.Services;
+using Microsoft.Extensions.Logging;
 
 namespace APPLICATION.Services;
 
-public class UserService : IUserService
+public class UserService(IUserRepository userRepository) : IUserService
 {
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<User> CreateAsync(User user)
+    {
+        var result = await _userRepository.CreateAsync(user);
+        return result;
+    }
+
+    public async Task<bool> DeleteAsync(int userId)
+    {
+        var result = await _userRepository.DeleteAsync(userId);
+        return result;
+    }
+
+    public Task<List<User>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
 }
--- BookABite_backend/DOMAIN/Services/IUserService.cs
+++ BookABite_backend/DOMAIN/Services/IUserService.cs
@@ -1,7 +1,14 @@
 ﻿
 
+using DOMAIN.Models;
+
 namespace DOMAIN.Services;
 
 public interface IUserService
 {
+    Task<User> GetByIdAsync(int userId);
+    Task<User> CreateAsync(User user);
+    Task<User> UpdateAsync(User user);
+    Task<bool> DeleteAsync(int userId);
+    Task<List<User>> GetAsync();
 }
