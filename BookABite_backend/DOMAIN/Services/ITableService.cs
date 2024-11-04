92757cae139dd6ab361ac05b21aef3ba82e44368
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-11-04 17:35:20 +0100

    KAN-32 TableService

---
+++ BookABite_backend/DOMAIN/Services/ITableService.cs
@@ -0,0 +1,18 @@
+﻿using DOMAIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMAIN.Services
+{
+    public interface ITableService
+    {
+        Task<Table> GetByIdAsync(int tableId);
+        Task<Table> CreateAsync(Table table);
+        Task<Table> UpdateAsync(Table table);
+        Task<bool> DeleteAsync(int tableId);
+        Task<List<Table>> GetAsync();
+    }
+}
