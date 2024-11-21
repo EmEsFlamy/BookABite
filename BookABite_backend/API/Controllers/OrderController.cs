5e32ae6502fa0fc17cdb3c48f192620a3dae542a
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-11-21 19:09:28 +0100

    KAN-36 Order Controller

---
+++ BookABite_backend/API/Controllers/OrderController.cs
@@ -0,0 +1,69 @@
+﻿using DOMAIN.Models;
+using DOMAIN.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IOrderService orderService) : ControllerBase
+{
+    private readonly IOrderService _orderService = orderService;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(Order order)
+    {
+        var result = await _orderService.CreateAsync(order);
+        if (result is null)
+        {
+            return BadRequest("Something went wrong during order creation!");
+        }
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByIdAsync(int orderId)
+    {
+        var result = await _orderService.GetByIdAsync(orderId);
+        if (result is null)
+        {
+            return NotFound("Order not found!");
+        }
+        return Ok(result);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAsync()
+    {
+        var result = await _orderService.GetAsync();
+        if (result is null)
+        {
+            return NotFound("Orders not found!");
+        }
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(Order order)
+    {
+        var result = await _orderService.UpdateAsync(order);
+        if (result is null)
+        {
+            return BadRequest("Cannot update this resource!");
+        }
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteAsync(int orderId)
+    {
+        var result = await _orderService.DeleteAsync(orderId);
+        if (result is false)
+        {
+            return NotFound("Order not found!");
+        }
+        return Ok(result);
+    }
+}
---
+++ BookABite_backend/DOMAIN/Services/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using DOMAIN.Models;
+
+namespace DOMAIN.Services;
+
+public interface IOrderService
+{
+    Task<Order> GetByIdAsync(int orderId);
+    Task<Order> CreateAsync(Order order);
+    Task<Order> UpdateAsync(Order order);
+    Task<bool> DeleteAsync(int orderId);
+    Task<List<Order>> GetAsync();
+}
