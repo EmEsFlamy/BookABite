e9c165bede64e5fe2a80af3fa12a2e62f7816bc1
EmEsFlamy <91785833+EmEsFlamy@users.noreply.github.com>
2024-12-06 16:51:20 +0100

    Update Cors

--- BookABite_backend/API/Program.cs
+++ BookABite_backend/API/Program.cs
@@ -79,12 +79,12 @@ app.UseExceptionHandler(opt => { });
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseCors();
 
 app.Run();
